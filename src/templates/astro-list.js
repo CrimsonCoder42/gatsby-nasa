21d9b5eb36306f1e1c823bf8ada2455813eb345b
CrimsonCoder42 <53884312+CrimsonCoder42@users.noreply.github.com>
2023-04-19 11:10:38 -0400

    comment update

--- src/templates/astro-list.js
+++ src/templates/astro-list.js
@@ -1,31 +1,43 @@
-import * as React from "react"
-import { graphql, Link } from "gatsby"
-import Layout from '../components/layout'
-import Pagination from '../components/pagination'
+// Import necessary libraries and components
+import * as React from "react";
+import { graphql, Link } from "gatsby";
+import Layout from "../components/layout";
+import Pagination from "../components/pagination";
 
-
+// AstroListPage component for rendering the list of images
 const AstroListPage = ({ data, pageContext }) => {
+  // Extract images data from the GraphQL query result
   const images = data?.allNasaJson?.nodes || [];
   console.log(pageContext);
+
   return (
     <Layout pageTitle="Archives">
       <ul>
+        {/* Map through images and create a list item for each image */}
         {images.map((image) => {
-          return <li key={image.id}><Link to={"/archives/"+image.date}>{image.date} {image.title}</Link></li>
+          return (
+            <li key={image.id}>
+              <Link to={"/archives/" + image.date}>
+                {image.date} {image.title}
+              </Link>
+            </li>
+          );
         })}
       </ul>
+      {/* Add Pagination component */}
       <Pagination
         currentPage={pageContext.currentPage}
         numPages={pageContext.numPages}
         pathPrefix={pageContext.pathPrefix}
       />
     </Layout>
-  )
-}
+  );
+};
 
+// GraphQL query to fetch image data with pagination
 export const query = graphql`
   query ($skip: Int!, $limit: Int!) {
-    allNasaJson(skip: $skip, limit: $limit, sort: {date: DESC}) {
+    allNasaJson(skip: $skip, limit: $limit, sort: { date: DESC }) {
       nodes {
         id
         hdurl
@@ -36,8 +48,11 @@ export const query = graphql`
       }
     }
   }
-`
+`;
 
-export default AstroListPage
+// Export the AstroListPage component as the default export
+export default AstroListPage;
 
-export const Head = () => <title>Home Page</title>
+// Define the Head component for setting the page title
+export const Head = () => <title>Home Page</title>;
+
