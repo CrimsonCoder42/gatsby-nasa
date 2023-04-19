c41e9d57f2417a83c32904437a402c3937a3f5ee
CrimsonCoder42 <53884312+CrimsonCoder42@users.noreply.github.com>
2023-04-19 11:03:05 -0400

    comment update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,26 @@
+// Import necessary libraries and components
 import * as React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import AstroMedia from "../components/astro-media";
 
+// Define the IndexPage component
 const IndexPage = ({ data }) => {
+  // Extract the APOD (Astronomy Picture of the Day) data
   const apod = data.allNasaJson.nodes[0];
+
+  // Render the component
   return (
     <Layout pageTitle="Astronomy Picture of the Day">
-      <h3>{apod.display_date}: {apod.title}</h3>
+      <h3>
+        {apod.display_date}: {apod.title}
+      </h3>
       <AstroMedia data={apod}></AstroMedia>
     </Layout>
   );
 };
 
+// Define the GraphQL query to fetch the data
 export const query = graphql`
   query IndexQuery {
     allNasaJson(limit: 1, sort: { date: DESC }) {
@@ -31,6 +39,9 @@ export const query = graphql`
   }
 `;
 
+// Export the IndexPage component as the default export
 export default IndexPage;
 
+// Define the Head component for setting the page title
 export const Head = () => <title>Home Page</title>;
+
