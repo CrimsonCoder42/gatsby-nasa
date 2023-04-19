4c2957d43ff47bbe8f4dd83c227fd3511c1350be
CrimsonCoder42 <53884312+CrimsonCoder42@users.noreply.github.com>
2023-04-19 11:07:30 -0400

    comment update

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,33 +1,34 @@
+// Import necessary libraries and components
 import React from "react";
 import { useState } from "react";
 import { graphql, Link } from "gatsby";
 import Layout from "../components/layout";
-import * as searchStyles from "./search.module.css"
+import * as searchStyles from "./search.module.css";
 
 // Utility function to lowercase the title and explanation for searching
 const normalizeNode = (node) => {
-    return {
-        ...node,
-        date: node.date,
-        title: node.title.toLowerCase(),
-        explanation: node.explanation.toLowerCase(),
-    }
-}
+  return {
+    ...node,
+    date: node.date,
+    title: node.title.toLowerCase(),
+    explanation: node.explanation.toLowerCase(),
+  };
+};
 
 // Utility function to match a node
 const matchNode = (node, terms) => {
   const { date, title, explanation } = node;
   return date.includes(terms) || title.includes(terms) || explanation.includes(terms);
-}
+};
 
 // Utility function to match nodes based on search terms
 const filterNodes = (nodes, terms) => {
-    if(!terms || terms.length === 0) {
-        return []
-    }
-    terms = terms.toLowerCase();
-    return nodes.map(normalizeNode).filter((node) => matchNode(node, terms));
-}
+  if (!terms || terms.length === 0) {
+    return [];
+  }
+  terms = terms.toLowerCase();
+  return nodes.map(normalizeNode).filter((node) => matchNode(node, terms));
+};
 
 // Search component
 const SearchPage = ({ data }) => {
@@ -39,18 +40,27 @@ const SearchPage = ({ data }) => {
   return (
     <Layout pageTitle="Search">
       <div className={searchStyles.searchBox}>
-        <input type="search" name="q" minLength="2" maxLength="100" style={{width:"100%"}} onChange={handleSearch} placeholder="Enter your search terms..."></input>
+        <input
+          type="search"
+          name="q"
+          minLength="2"
+          maxLength="100"
+          style={{ width: "100%" }}
+          onChange={handleSearch}
+          placeholder="Enter your search terms..."
+        ></input>
       </div>
-      <div>
-        {terms && <p>{matches.length} results out of {nodes.length}</p>}
-      </div>
+      <div>{terms && <p>{matches.length} results out of {nodes.length}</p>}</div>
       <ul>
         {matches.map((node) => {
           return (
-              <li key={node.id}>
-                  <Link to={"/archives/" + node.date}>{node.date} {node.title}</Link>
-              </li>)
-          })}
+            <li key={node.id}>
+              <Link to={"/archives/" + node.date}>
+                {node.date} {node.title}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </Layout>
   );
@@ -76,6 +86,9 @@ export const query = graphql`
   }
 `;
 
+// Export the SearchPage component as the default export
 export default SearchPage;
 
+// Define the Head component for setting the page title
 export const Head = () => <title>Search Page</title>;
+
